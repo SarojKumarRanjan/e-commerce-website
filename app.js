101ccf8118b90d69aedf00368f3022a233c2f08e
Snehakumari06 <119296074+Snehakumari06@users.noreply.github.com>
2024-04-21 13:05:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 5000; // Use the dynamic port assigned by Herok
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://snehakumari:@123Aman@sneha.tbgpj9n.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 // Define user schema
