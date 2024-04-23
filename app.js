5efd8db0bb2e58c5971bfb18435eeef9fb2d9caf
Snehakumari06 <119296074+Snehakumari06@users.noreply.github.com>
2024-04-23 06:29:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,10 +41,10 @@ const app = express();
 app.use(express.static('public'));
 const port = process.env.PORT || 5000; // Use the dynamic port assigned by Heroku or default to 5000
 
-app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: '*', // Allow requests from this origin
+  origin: ['https://e-commerce-website-ft4k.onrender.com', '*'],// Allow requests from this origin
   methods: ['GET', 'POST'],      // Allow these HTTP methods
   allowedHeaders: ['Content-Type'], // Allow these headers
 }));
